7f39648520e6287f4002bc77037bc97e1208203d
betower24 <127764841+betower24@users.noreply.github.com>
2025-12-25 15:30:04 -0600

    Corregir vista punto_de_venta

--- core/settings.py
+++ core/settings.py
@@ -138,3 +138,5 @@ TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', '')
 TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', '')
 TWILIO_WHATSAPP_FROM = os.environ.get('TWILIO_WHATSAPP_FROM', 'whatsapp:+14155238886')
 WHATSAPP_TO = os.environ.get('WHATSAPP_TO', 'whatsapp:+525618218562')  # TU NÚMERO
+DEBUG = False  # Debe estar en False en producción
+ALLOWED_HOSTS = ['mi-pos-django.onrender.com', 'localhost']
